08c30f07467fb5ddb33de17ffbe601c3ca284c3e
Fernando Flores <ferjflores@gmail.com>
2020-05-19 15:03:31 -0400

    Daemon keep connection

---
+++ keep.py
@@ -0,0 +1,59 @@
+# #!/usr/bin/python3
+
+import logging
+import time
+from daemon import runner
+from library import check_dns_resolution, is_connected, reset
+
+REMOTE_SERVER_NAME = "one.one.one.one"
+REMOTE_SERVER_IP = "1.1.1.1"
+DEFAULT_ROUTER_URL = "http://localhost:9080"
+# DEFAULT_ROUTER_URL = "http://192.168.88.1"
+DEFAULT_ADMIN_PW = "admin"
+
+
+class Runner(runner.DaemonRunner):
+    def _open_streams_from_app_stream_paths(self, app):
+        self.daemon_context.stdin = open(app.stdin_path, 'rt')
+        self.daemon_context.stdout = open(app.stdout_path, 'wb+', buffering=0)
+        self.daemon_context.stderr = open(app.stderr_path, 'wb+', buffering=0)
+
+
+class App:
+    def __init__(self):
+        self.stdin_path = '/dev/null'
+        self.stdout_path = '/dev/tty'
+        self.stderr_path = '/dev/tty'
+        self.pidfile_path = '/tmp/keep_connection.pid'
+        self.pidfile_timeout = 5
+        self.url = DEFAULT_ROUTER_URL
+        self.password = DEFAULT_ADMIN_PW
+        self.restart = False
+
+    def run(self):
+        i = 0
+        while True:
+            check_dns, message = check_dns_resolution(REMOTE_SERVER_NAME)
+            check_connection, message = is_connected(REMOTE_SERVER_IP)
+
+            if not check_connection and self.restart:
+                logger.warning("reset connection")
+                message = reset(self.url, self.password)
+                logger.info(message)
+            else:
+                logger.info("Connection working")
+            time.sleep(300)
+
+
+if __name__ == '__main__':
+    app = App()
+    logger = logging.getLogger("keep_connection")
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(message)s")
+    handler = logging.FileHandler("/tmp/keep_connection.log")
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    serv = Runner(app)
+    serv.daemon_context.files_preserve = [handler.stream]
+    serv.do_action()
---
+++ library/__init__.py
@@ -0,0 +1,78 @@
+import socket
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.alert import Alert
+from selenium.common.exceptions import TimeoutException
+
+
+def reset(url, pw):
+    message = ''
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    options.add_argument('disable-dev-shm-usage')
+    options.add_argument('no-sandbox')
+
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+    try:
+        assert "3G" in driver.title
+    except AssertionError:
+        return "Error connecting to {}".format(url)
+
+    password = driver.find_element_by_id("password")
+    password.send_keys(pw, Keys.ENTER)
+    wait = WebDriverWait(driver, 3)
+
+    try:
+        wait.until(EC.alert_is_present())
+
+        alert = driver.switch_to.alert
+        alert.accept()
+        message += 'Incorrect Password'
+        return message
+    except TimeoutException as error:
+        message += 'password accepted\n'
+
+    driver.switch_to.frame('listfrm')
+    selector_advanced_configuration = "labContent4"
+    # wait.until(EC.visibility_of_element_located(By.ID(selector_advanced_configuration)))
+    advanced_configuration = driver.find_element_by_id(selector_advanced_configuration)
+    advanced_configuration.click()
+
+    driver.switch_to.default_content()
+    driver.switch_to.frame('basefrm')
+    system = driver.find_element_by_id('labContent2')
+    system.click()
+
+    reboot = driver.find_element_by_id('labContent4')
+    reboot.click()
+
+    reboot_confirm = driver.find_element_by_id('labControl2')
+    reboot_confirm.click()
+    Alert(driver).accept()
+    message = "\nReboot successful"
+    driver.close()
+    return message
+
+def check_dns_resolution(hostname):
+    message = ''
+    try:
+        socket.gethostbyname(hostname)
+        return True, message
+    except Exception as error:
+        message = error
+    return False, message
+
+
+def is_connected(host):
+    message = ''
+    try:
+        s = socket.create_connection((host, 80), 2)
+        s.close()
+        return True, message
+    except Exception as error:
+        message = error
+    return False, message
